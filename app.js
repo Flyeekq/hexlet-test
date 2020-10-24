da61c070c8c78e1d8d3c061e84ec2dfa5b1eb6be
Max Kondrashenko <flyeekq@gmail.com>
2020-10-24 23:19:45 +0300

    added lodash

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-const obj = { name: "Vasya", age: 32, height: 170 };
-console.log(obj.name);
+var _ = require("lodash");
+
+const text1 = "";
+const text2 = "one two three two ONE one wow";
+const text3 = "another one sentence with strange Words words";
+
+const countWords = (text) => {
+  let result = {};
+  if (text === "") {
+    return result;
+  } else {
+    let wordsArr = _.words(text.toLowerCase());
+    for (word of wordsArr) {
+      result[word] = result[word] + 1 || 1;
+    }
+    return result;
+  }
+};
+
+//console.log(countWords(text1));
+console.log(countWords(text2));
+console.log(countWords(text3));
